56ca59ef5b756ae07a3bf263149228064beb33a3
troitonn <150198515+troitonn@users.noreply.github.com>
2025-08-14 12:30:54 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,16 +7,18 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 8080
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger()
   ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      "@": path.resolve(__dirname, "src")
+    }
   },
+  build: {
+    target: "esnext" // garante compatibilidade moderna para Netlify
+  }
 }));
